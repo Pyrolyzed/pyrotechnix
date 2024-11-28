801e88474af7db86eeb8c45a44b7416d97617340
Pyrolyzed <pyrolyzed@proton.me>
2024-11-28 17:34:52 -0600

    Changed mounting paramaters

--- profiles/laptop/configuration.nix
+++ profiles/laptop/configuration.nix
@@ -64,7 +64,7 @@
     device = "//192.168.1.200/Storage";
     fsType = "cifs";
     # Plain text password because I'm lazy and also because it's not exposed to the internet and also I don't use it anywhere else.
-    options = [ "uid=1000" "username=pyro" "password=spoons" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60" "x-systemd.device-timeout=5s" "x-systemd.mount-timeout=5s" ];
+    options = [ "uid=1000" "username=pyro" "password=spoons" "x-systemd.idle-timeout=60" "x-systemd.device-timeout=5s" "x-systemd.mount-timeout=5s" ];
   };
 
   fileSystems."/home/pyro/Storage" = {
