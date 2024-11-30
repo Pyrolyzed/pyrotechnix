5576d756d2d4aa24a6bfb3918494462d0f43e8c7
Pyrolyzed <pyrolyzed@proton.me>
2024-11-30 10:33:27 -0600

    Changed cursor

--- profiles/desktop/home.nix
+++ profiles/desktop/home.nix
@@ -40,12 +40,12 @@
     };
   };
 
-  home.pointerCursor = {
-    gtk.enable = true;
-    package = pkgs.bibata-cursors;
-    name = "Bibata-Modern-Classic";
-    size = 24;
-  };
+ # home.pointerCursor = {
+ #   gtk.enable = true;
+ #   package = pkgs.bibata-cursors;
+ #   name = "Bibata-Modern-Classic";
+ #   size = 24;
+ # };
 
   home.username = "pyro";
   home.homeDirectory = "/home/pyro";
