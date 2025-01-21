7a97db85d81ace4cae1a3ccd7616dfa097294949
Pyrolyzed <pyrolyzed@proton.me>
2025-01-21 17:25:03 -0600

    Added management of userDirs to default home-manager configuration

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,5 +1,20 @@
 { config, lib, pkgs, user, ... }:
 {
+  xdg.userDirs = {
+    enable = true;
+    documents = "/home/${user}/Storage/Media/Documents";
+    download = "/home/${user}/Storage/Downloads";
+    pictures = "/home/${user}/Storage/Media/Pictures";
+    videos = "/home/${user}/Storage/Media/Videos";
+    extraConfig = {
+      XDG_PROJECTS_DIR = "/home/${user}/Projects";
+    };
+    publicShare = null;
+    desktop = null;
+    music = null;
+    templates = null;
+  };
+
   home = {
     stateVersion = "24.05";
     username = user;
