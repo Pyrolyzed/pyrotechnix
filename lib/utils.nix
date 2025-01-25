36da32069a7da9e57196fb77689d66fe3bd6263d
Pyrolyzed <pyrolyzed@proton.me>
2025-01-24 18:15:08 -0600

    Changed hyprland module Added isEmptyList function

--- lib/utils.nix
+++ lib/utils.nix
@@ -3,6 +3,7 @@ lib.extend (
   _: libprev: {
     custom = rec {
       toOneWord = string: libprev.replaceStrings [" "] [""] string;
+      isEmptyList = list: (libprev.lists.compareLists libprev.compare list [ ]) == 1;
     };
   }
 )
