37612e495a1f7622c7c5740e7b1568be9930280f
Pyrolyzed <pyrolyzed@proton.me>
2025-03-12 00:05:47 -0500

    Removed redundant code

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -7,8 +7,10 @@
   ...
 }@args:
 let
+  desktopName = "emperor";
+  laptopName = "duke";
   isServer = host: lib.strings.hasPrefix "homeserver" host;
-  isPersonal = host: host == "emperor" || host == "duke";
+  isPersonal = host: host == desktopName || host == laptopName;
   mkConfiguration =
     host:
     {
@@ -19,20 +21,20 @@ let
       specialArgs = specialArgs // rec {
         inherit host user;
         isNixOS = true;
-        isDesktop = host == "emperor";
-        isLaptop = host == "duke";
+        isDesktop = host == desktopName;
+        isLaptop = host == laptopName;
         isPersonal = isDesktop || isLaptop;
         isVm = host == "vm";
         isServer = isServer host;
       };
 
       modules = modules ++ [
-        (if isServer host then ./homeserver/${host}/configuration.nix else ./${host}/configuration.nix) # Host specific configuration
-        (if isServer host then ./homeserver/${host}/hardware.nix else ./${host}/hardware.nix) # Host hardware configuration
-        (if isServer host then ./homeserver else { }) # Common homeserver configuration
+        (if isServer host then ./homeserver/${host}/configuration.nix else ./${host}/configuration.nix) # Host specific configuration.
+        (if isServer host then ./homeserver/${host}/hardware.nix else ./${host}/hardware.nix) # Host hardware configuration.
+        (if isServer host then ./homeserver else { }) # Common homeserver configuration.
         (if isPersonal host then ../default/personal else { })
-        ../default/nixos # Default nixos configuration
-        ../overlays # Access to overlays
+        ../default/nixos # Default nixos configuration.
+        ../overlays # Access to overlays.
         ../modules/nixos
         inputs.home-manager.nixosModules.home-manager
         {
@@ -44,17 +46,17 @@ let
             extraSpecialArgs = specialArgs // {
               inherit host user;
               isNixOS = true;
-              isDesktop = host == "emperor";
-              isLaptop = host == "duke";
+              isDesktop = host == desktopName;
+              isLaptop = host == laptopName;
               isVm = host == "vm";
               isServer = lib.strings.hasPrefix "homeserver" host;
             };
 
             users.${user} = {
               imports = [
-                (if isServer host then ./homeserver/${host}/home.nix else ./${host}/home.nix) # Host specific home configuration
+                (if isServer host then ./homeserver/${host}/home.nix else ./${host}/home.nix) # Host specific home configuration.
                 (if isPersonal host then ../default/personal/home.nix else { })
-                ../default/home-manager # Default home configuration
+                ../default/home-manager # Default home configuration.
                 ../modules/homeManager
               ];
             };
@@ -69,7 +71,7 @@ let
     range: lib.attrsets.genAttrs (getHomeservers range) (name: mkConfiguration name { });
 in
 {
-  emperor = mkConfiguration "emperor" { };
-  duke = mkConfiguration "duke" { };
+  emperor = mkConfiguration desktopName { };
+  duke = mkConfiguration laptopName { };
 }
 // mkHomeservers (lib.range 1 4)
