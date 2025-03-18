9f675e1fde46dc251185b04f3d6eb03ea58a4a91
Pyrolyzed <pyrolyzed@proton.me>
2025-03-17 21:02:22 -0500

    Better code, thanks iynaix

--- overlays/esde.nix
+++ overlays/esde.nix
@@ -1,24 +1,14 @@
 {
-  pkgs,
   pkgs-stable,
   ...
 }:
 {
-  # Temporary fix for https://github.com/NixOS/nixpkgs/issues/380330
+  # Fix for ES-DE not building.
   nixpkgs.overlays = [
     (final: prev: {
-      # emulationstation-de = pkgs-stable.emulationstation-de;
-      emulationstation-de = prev.emulationstation-de.overrideAttrs (old: {
-        buildInputs = map (
-          pkg:
-          if pkg == pkgs.icu then
-            pkgs-stable.icu
-          else if pkg == pkgs.libgit2 then
-            pkgs-stable.libgit2
-          else
-            pkg
-        ) old.buildInputs;
-      });
+      emulationstation-de = prev.emulationstation-de.override {
+        inherit (pkgs-stable) icu libgit2;
+      };
     })
   ];
 }
