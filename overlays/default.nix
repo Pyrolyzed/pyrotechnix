332b06bfdb914653bcb8d289d881fd801feedb14
Pyrolyzed <pyrolyzed@proton.me>
2025-04-13 03:25:38 -0500

    Clarity fix

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,7 +6,16 @@
 {
   imports = [
     ./esde.nix
-    ./custom.nix
   ];
 
+  nixpkgs.overlays = [
+    (final: prev: {
+      custom =
+        (prev.custom or { })
+        // (import ../packages {
+          inherit (prev) pkgs;
+          inherit inputs;
+        });
+    })
+  ];
 }
